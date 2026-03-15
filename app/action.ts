0dac4d8dcb0b872f3f36835faf3127e2d4d9ae68
Rridwan01 <114353347+Rridwan01@users.noreply.github.com>
2026-03-15 21:09:03 +0100

    update contact

--- app/action.ts
+++ app/action.ts
@@ -3,7 +3,6 @@
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-// The single owner's email is stored securely in your .env file
 const OWNER_EMAIL = process.env.OWNER_EMAIL as string; 
 
 export async function sendContactEmail(formData: FormData) {
